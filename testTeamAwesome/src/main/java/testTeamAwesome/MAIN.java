988495c8c1daaafecb026594d74f08d5263cbd0c
Maria Rodriguez <rodriguezmarialoren@gmail.com>
2019-02-10 16:22:19 -0500

    Maria's test

--- testTeamAwesome/src/main/java/testTeamAwesome/MAIN.java
+++ testTeamAwesome/src/main/java/testTeamAwesome/MAIN.java
@@ -5,6 +5,7 @@ public class MAIN {
 	public static void main(String[] args) {
 		System.out.println("James edited this file. You guys, we're awesome");
 
+		System.out.println("HELLO this is Maria!");
 	}
 
 }
